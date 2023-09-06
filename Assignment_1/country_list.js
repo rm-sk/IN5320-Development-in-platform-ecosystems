2e812b45651f73f0787ce2b9135edacb46303b27
rm-sk <rm.sk@outlook.com>
2023-09-06 22:31:56 +0200

    Started implementing search functionality

--- Assignment_1/country_list.js
+++ Assignment_1/country_list.js
@@ -1,10 +1,11 @@
+const fullList = document.getElementById("fullList")
+
 
 // Adding a new country to the list
 function add_country(){
-    const country = document.querySelector("textarea").value;
-    document.querySelector("textarea").value = "";
+    const country = document.getElementById("countryInput").value;
+    document.getElementById("countryInput").value = "";
 
-    const list = document.querySelector("ul");
     const newLi = document.createElement("li");
 
     const delBtn = document.createElement("button");
@@ -13,7 +14,7 @@ function add_country(){
     newLi.appendChild(document.createTextNode(country));
     newLi.appendChild(delBtn);
 
-    list.appendChild(newLi); 
+    fullList.appendChild(newLi); 
     
     // Deleting a country from the list
     delBtn.addEventListener("click", () => {
@@ -38,3 +39,28 @@ function delete_country(country){
 function starts_with(element, searchWord){
     return element.startsWith(searchWord);
 }
+
+// Filters an array baseed on the starts_with function
+function filter_array(list, searchWord){
+    filtered = list.filter(starts_with);
+}
+
+// Search bar implementation
+searchBar = document.getElementById("searchBar");
+
+searchBar.addEventListener("keyup", () => {
+    if (searchBar.value != "") {
+        const listArray = fullList.getElementsByTagName("li");
+        const result = filter_array(listArray, searchBar.value);
+        result.setAttribute("id", "searchList");
+        fullList.style.display = "none";
+        document.querySelector("body").appendChild(result);
+    } else {
+        fullList.style.display = "block";
+        document.querySelector("searchList").style.display = "none";
+    }
+})
+
+
+
+
