0ab3157e1ebc18ad58dbb02368684df58ae8d752
rm-sk <rm.sk@outlook.com>
2023-09-06 18:54:25 +0200

    Deleting countries

--- Assignment_1/country_list.js
+++ Assignment_1/country_list.js
@@ -1,18 +1,34 @@
-function add_country(){
-    country = document.querySelector("textarea").value;
-    document.querySelector("textarea").value = "";
 
-    if (country) {
-        console.log(country);
-    }
-    
+// Adding a new country to the list
+function add_country(){
+    const country = document.querySelector("textarea").value;
+    document.querySelector("textarea").value = "";
 
     const list = document.querySelector("ul");
     const newLi = document.createElement("li");
 
+    const delBtn = document.createElement("button");
+    delBtn.innerText = "Delete";
+
     newLi.appendChild(document.createTextNode(country));
-    list.appendChild(newLi);
+    newLi.appendChild(delBtn);
 
+    list.appendChild(newLi); 
     
+    // Deleting a country from the list
+    delBtn.addEventListener("click", () => {
+        delete_country(newLi);
+    })
+}
 
+// Deleting a country from the list
+function delete_country(country){
+    const list = document.querySelector("ul");
+    const elements = document.querySelectorAll("li");
+
+    for (let i=0; i<elements.length; i++) {
+        if (elements[i] == country) {
+            list.removeChild(elements[i]);
+        }
+    }
 }
