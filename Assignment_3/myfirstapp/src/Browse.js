c3ae825f57e183b02c05800b45ec6df691408b25
rm-sk <rm.sk@outlook.com>
2023-10-04 19:11:37 +0200

    Added table to browse

--- Assignment_3/myfirstapp/src/Browse.js
+++ Assignment_3/myfirstapp/src/Browse.js
@@ -1,41 +1,54 @@
 import React from 'react'
 import { useDataQuery } from '@dhis2/app-runtime'
+import { CircularLoader } from '@dhis2/ui'
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableCellHead,
+	TableFoot,
+	TableHead,
+	TableRow,
+	TableRowHead,
+} from '@dhis2/ui'
+
+
 
 const dataQuery = {
-    dataSets: {
-        resource: 'dataSets/aLpVgfXiz0f',
-        params: {
-            fields: [
-                'name',
-                'id',
-                'dataSetElements[dataElement[id, displayName]',
-            ],
-        },
-    },
-    dataValueSets: {
-        resource: 'dataValueSets',
-        params: {
-            orgUnit: 'KiheEgvUZ0i',
-            dataSet: 'aLpVgfXiz0f',
-            period: '2020',
-        },
-    },
+	dataSets: {
+		resource: 'dataSets/aLpVgfXiz0f',
+		params: {
+			fields: [
+				'name',
+				'id',
+				'dataSetElements[dataElement[id, displayName]',
+			],
+		},
+	},
+	dataValueSets: {
+		resource: 'dataValueSets',
+		params: {
+			orgUnit: 'KiheEgvUZ0i',
+			dataSet: 'aLpVgfXiz0f',
+			period: '2020',
+		},
+	},
 }
 
 function mergeData(data) {
-  return data.dataSets.dataSetElements.map(d => {
-      let matchedValue = data.dataValueSets.dataValues.find(dataValues => {
-          if (dataValues.dataElement == d.dataElement.id) {
-              return true
-          }
-      })
+	return data.dataSets.dataSetElements.map(d => {
+		let matchedValue = data.dataValueSets.dataValues.find(dataValues => {
+			if (dataValues.dataElement == d.dataElement.id) {
+				return true
+			}
+		})
 
-      return {
-          displayName: d.dataElement.displayName,
-          id: d.dataElement.id,
-          value: matchedValue.value,
-      }
-  })
+		return {
+			displayName: d.dataElement.displayName,
+			id: d.dataElement.id,
+			value: matchedValue.value,
+		}
+	})
 }
 
 export function Browse() {
@@ -46,12 +59,34 @@ export function Browse() {
     }
 
     if (loading) {
-        return <span>Loading...</span>
+      return <CircularLoader/>
     }
 
     if (data) {
-        console.log(data)
-    }
+        let mergedData = mergeData(data)
+        console.log(mergedData)
 
-    return <h1>Browse</h1>
+		return (
+			<Table>
+					<TableHead>
+						<TableRowHead>
+							<TableCellHead>Display Name</TableCellHead>
+							<TableCellHead>Value</TableCellHead>
+							<TableCellHead>ID</TableCellHead>
+						</TableRowHead>
+					</TableHead>
+					<TableBody>
+						{mergedData.map(row => {
+							return (
+								<TableRow key={row.id}>
+									<TableCell>{row.displayName}</TableCell>
+									<TableCell>{row.value}</TableCell>
+									<TableCell>{row.id}</TableCell>
+								</TableRow>
+							)
+						})}
+					</TableBody>
+				</Table>
+		)
+    }
 }
