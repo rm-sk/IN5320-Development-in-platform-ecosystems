1e3e34db117e46786dbd4d66b92494f5c203dad8
rm-sk <rm.sk@outlook.com>
2023-10-04 18:37:18 +0200

    Added dataQuery to Browse

--- Assignment_3/myfirstapp/src/Browse.js
+++ Assignment_3/myfirstapp/src/Browse.js
@@ -1,5 +1,26 @@
-import React from "react";
+import React from 'react'
+
+const dataQuery = {
+    dataSets: {
+        resource: 'dataSets/aLpVgfXiz0f',
+        params: {
+            fields: [
+                'name',
+                'id',
+                'dataSetElements[dataElement[id, displayName]',
+            ],
+        },
+    },
+    dataValueSets: {
+        resource: 'dataValueSets',
+        params: {
+            orgUnit: 'KiheEgvUZ0i',
+            dataSet: 'aLpVgfXiz0f',
+            period: '2020',
+        },
+    },
+}
 
 export function Browse() {
-  return <h1>Browse</h1>;
+    return <h1>Browse</h1>
 }
