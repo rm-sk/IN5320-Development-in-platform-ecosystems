1659faec7c19deea4f36d0f587debaf39fa90434
rm-sk <rm.sk@outlook.com>
2023-10-04 19:39:11 +0200

    Added query to datasets

--- Assignment_3/myfirstapp/src/Datasets.js
+++ Assignment_3/myfirstapp/src/Datasets.js
@@ -1,5 +1,31 @@
 import React from 'react'
+import { useDataQuery } from '@dhis2/app-runtime'
 
+const dataQuery = {
+    dataSets: {
+        resource: 'dataSets',
+        id: ({ id }) => id,
+        params: {
+            fields: ['dataSetElements[dataElement[id, displayName,created]'],
+            paging: false,
+        },
+    },
+}
+  
 export function Datasets() {
-	return <span>Data</span>
+    const { loading, error, data, refetch } = useDataQuery(dataQuery)
+
+    if (error) {
+        return <span>ERROR: {error.message}</span>
+    }
+
+    if (loading) {
+        return <span>Loading...</span>
+    }
+
+    if (data) {
+        console.log("API response:",data)
+        //To-do: return a component using the data response 
+        return <span>Data</span>
+    }
 }
